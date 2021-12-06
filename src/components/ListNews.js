8ef330bebada33f0c0e31cd9c08d49f506272f70
Gonzo <gvidal.industrial@gmail.com>
2021-12-05 19:50:31 -0500

    Fix typeerror in News component

--- src/components/ListNews.js
+++ src/components/ListNews.js
@@ -3,9 +3,12 @@ import News from './News';
 import PropTypes from 'prop-types';
 
 const ListNews = ({news}) => {
+  console.log(news);
+  if(news?.length === 0) return null;
+
   return(
     <div className="row">
-      { news.map(newsElement => (
+      { news?.map(newsElement => (
         <News 
           key={newsElement.url}
           newsElement={newsElement}
